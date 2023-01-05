325c0f741c9e677a8602bb9749ccea1f1298bfe8
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-01-06 04:47:18 +0700

    adding logo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { ReactComponent as Logo } from '../assets/logo/title.svg'
 
 const Sidebar = () => {
     return (
-        <div className='flex flex-col w-[250px] fixed top-0 left-0 bottom-0 bg-[#E1F2FB]' >
+        <div className='flex flex-col items-center w-[250px] fixed top-0 left-0 bottom-0 bg-[#E1F2FB]' >
+            <div>
+                <Logo className='w-[150px] mb-5' />
+            </div>
             <NavLink to='/dashboard'>
                 Dashboard
             </NavLink>
