49998c6ec3cbe78418bb83ff0dd689ec647315c7
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-01-06 04:01:34 +0700

    fix error

--- src/components/Headbar.js
+++ src/components/Headbar.js
@@ -8,6 +8,8 @@ const Headbar = ({ data }) => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
+    const photo = data?.data.id_staff.photo
+
     const handleLogout = () => {
         dispatch(logout())
         navigate('/')
@@ -16,11 +18,17 @@ const Headbar = ({ data }) => {
         <nav className="bg-[#F1F9F9] border-gray-200 px-2 sm:px-4 py-2.5">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
                 <a href="https://flowbite.com/" className="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="" />
+                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
                 </a>
                 <button onClick={handleLogout} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg " aria-controls="navbar-default" aria-expanded="false">
-                    <img className='w-10 h-10 rounded outline-0' src={data?.data.id_staff !== null ? `http://localhost:5000/${data?.data.id_staff.photo}` : Photo} alt='' />
+                    {photo === undefined ? '' :
+                        < img
+                            className='w-10 h-10 rounded outline-0'
+                            src={photo !== null ? `http://localhost:5000/${photo}` : Photo}
+                            alt={photo}
+                        />
+                    }
                 </button>
             </div>
         </nav>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import Headbar from './Headbar'
 import Sidebar from './Sidebar'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import jwtDecode from 'jwt-decode'
+import { useNavigate } from 'react-router-dom'
+import { logout } from '../store/slices/AuthSlice'
 
 const Layout = () => {
     const { token } = useSelector(state => state.authUser)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const [id, setId] = useState(null)
     const [data, setData] = useState(null)
     const getId = id?.userId
+
+
+
     useEffect(() => {
         const getUser = async () => {
             try {
                 const decode = await jwtDecode(JSON.stringify(token))
                 setId(decode)
+                if (decode.exp * 1000 < Date.now()) {
+                    dispatch(logout())
+                    navigate('/')
+                    alert('expired token')
+                }
             } catch (error) {
-
+                return
             }
         }
         getUser()
         const getData = async () => {
             try {
+                if (getId === undefined) { return null }
                 const response = await axios.get(`http://localhost:5000/api/user/${getId}`, {
                     headers: {
                         authorization: `Bearer ${token}`
@@ -34,7 +47,7 @@ const Layout = () => {
 
         }
         getData()
-    }, [token, getId])
+    }, [token, getId, dispatch, navigate])
 
     return (
         <div>
