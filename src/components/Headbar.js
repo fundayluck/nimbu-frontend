b9c9b6fd30fe9c73b262018ec3e193530d7cf755
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-01-06 10:33:47 +0700

    dropdown

--- src/components/Headbar.js
+++ src/components/Headbar.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Photo from '../assets/images/photo.jpg'
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { BiChevronDown, BiChevronLeft } from 'react-icons/bi'
 import { logout } from '../store/slices/AuthSlice';
 
 const Headbar = ({ data }) => {
+    const [isOpen, setIsOpen] = useState(false)
+
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
     const handleLogout = () => {
+        setIsOpen(false)
         dispatch(logout())
         navigate('/')
     }
+
+    const handleDropdown = () => {
+        !isOpen ? setIsOpen(true) : setIsOpen(false)
+    }
+
     return (
         <nav className="bg-[#F1F9F9] border-gray-200 px-2  fixed w-full shadow">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
@@ -19,7 +28,7 @@ const Headbar = ({ data }) => {
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
                 </a>
-                <button onClick={handleLogout} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg " aria-controls="navbar-default" aria-expanded="false">
+                <button onClick={handleDropdown} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg " aria-controls="navbar-default" aria-expanded="false">
                     {data?.data.id_staff === undefined && data?.data.id_staff.photo === undefined ?
                         < img
                             className='w-10 h-10 rounded outline-0'
@@ -27,19 +36,42 @@ const Headbar = ({ data }) => {
                             alt='avatar'
                         />
                         :
-                        < img
-                            className='w-10 h-10 rounded outline-0'
-                            src={
-                                data?.data.id_staff !== null
-                                    ? `http://localhost:5000/${data?.data.id_staff.photo}`
-                                    : Photo
+                        <>
+                            < img
+                                className='w-10 h-10 rounded outline-0'
+                                src={
+                                    data?.data.id_staff !== null
+                                        ? `http://localhost:5000/${data?.data.id_staff.photo}`
+                                        : Photo
+                                }
+                                alt='avatar'
+                            />
+                            {
+                                isOpen ?
+                                    <BiChevronDown className='ease-out text-xl' />
+                                    :
+                                    <BiChevronLeft className='ease-out text-xl' />
                             }
-                            alt='avatar'
-                        />
+
+                        </>
                     }
-
-
                 </button>
+                <div className={`absolute transition duration-200 ease-out right-0 z-10 w-30 mt-[140px] mr-4 origin-top-right bg-[#C2A3A1] border border-gray-100 rounded-b-xl rounded-l-xl shadow-lg ${isOpen ? 'scale-1' : 'scale-0'}`}>
+                    <div className="p-2">
+                        <button
+                            onClick={() => setIsOpen(false)}
+                            className="block px-4 py-2  text-sm text-gray-500 rounded-lg  hover:text-gray-700"
+                        >
+                            Account Setting
+                        </button>
+                        <button
+                            onClick={handleLogout}
+                            className="block px-4 py-2  text-sm text-gray-500 rounded-lg hover:text-gray-700"
+                        >
+                            Logout
+                        </button>
+                    </div>
+                </div>
             </div>
         </nav>
     )
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -55,20 +55,20 @@ const Login = () => {
                             <form className='flex flex-col' onSubmit={doLogin}>
                                 <input
                                     type='email'
-                                    className='bg-[#F1F9F9] border-b-2 w-[320px] px-2 my-2 outline-0 leading-loose'
+                                    className='border-b-2 w-[320px] px-2 my-2 outline-0 leading-loose'
                                     placeholder='Email'
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                 />
                                 <input
                                     type='password'
-                                    className='bg-[#F1F9F9] border-b-2 w-[320px] px-2 my-2 outline-0 leading-loose'
+                                    className='border-b-2 w-[320px] px-2 my-2 outline-0 leading-loose'
                                     placeholder='password'
                                     value={password}
                                     onChange={(e) => setPassword(e.target.value)}
                                 />
 
-                                <div className={`transition duration-200 ease-out bg-[#FDE4E1] border text-red-700  rounded relative  ${err ? 'scale-10 mt-[12px] px-4 py-3' : 'scale-0 '}`} role="alert">
+                                <div className={`transition duration-200 ease-out bg-[#FDE4E1] border text-red-700  rounded relative  ${err ? 'scale-10 mt-[12px] px-4 py-3' : 'scale-0'}`} role="alert">
                                     <span className="block sm:inline">{errorMsg}.</span>
                                     <span className="absolute top-0 bottom-0 right-0 px-4 py-3" >
                                         <svg className="fill-current h-6 w-6 text-black-500" onClick={clearMessage} role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
