d74ea465f72630fa1b2bd8196f14adb2e998f191
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-01-17 21:37:15 +0700

    adding props disabled

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ name, className, onClick, type }) => {
+const Button = ({ name, className, onClick, type, disabled }) => {
     return (
-        <button type={type} className={className} onClick={onClick}>
+        <button type={type} disabled={disabled} className={className} onClick={onClick}>
             {name}
         </button>
     )
